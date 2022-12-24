8a4ca0db3454485385331b9c20fc259c84eaccc6
LucassantoSCruz <lucas.cruz162@etec.sp.gov.br>
2022-12-24 00:15:08 -0300

    Atualização
    Adição de imagens e conclusão do modelo de "Cards".

--- Components.js
+++ Components.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 
 const BelezuraCard = () => {
     return (
         <View style={style.view}>
-            <Text style={style.text1}>
+
+            <Image
+            source={require('./assets/salao.png')}
+            style={style.image}
+            ></Image>
+
+            <Text
+            style={style.text1}>
                 Criação de "Card"
             </Text>
-            <Text style={style.text2}>
+
+            <Text
+            style={style.text2}>
                 O próximo passo é dar uma organizada no código e colocar uma imagem nesse card. Já estou me sentindo maluco 🤪
             </Text>
+
         </View>
     )
 }
@@ -20,15 +30,19 @@ const style = StyleSheet.create({
     view: {
         width: telaWidht - 30,
         backgroundColor: 'white',
-        height: 200,
+        height: 300,
+        borderRadius: 20,
         padding: 20,
-        borderRadius: 20
+        margin: 20
     },
     text1: {
         fontWeight: 'bold',
         fontSize: 25
     },
-    text2: {
+    image: {
+        widht: telaWidht - 30,
+        height: 150,
+        borderRadius: 10
     },
 })
 
--- TelaSecundaria.js
+++ TelaSecundaria.js
@@ -1,22 +1,44 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import BelezuraCard from './Components';
 
 const TelaSecundaria = ({}) => {
   return (
     <View
       style={style.view}>
+      
+    <TouchableOpacity>
+      <Image
+      style={style.image2}
+      source={require('./assets/lupa1.png')}
+      />
+    </TouchableOpacity>
 
     <Image
       style={style.image}
       source={require('./assets/img1.png')}/>
 
     <Text
-      style={style.text}>
-        Corte Simples
+      style={style.text2}>
+        Cabeleireiro 
     </Text>
-    
-    <BelezuraCard>
-    </BelezuraCard>
+
+    <Text
+      style={style.text2}>
+        Manicure 
+    </Text>
+
+    <Text
+      style={style.text2}>
+        Maquiador 
+    </Text>
+
+    <BelezuraCard/>
+
+    <BelezuraCard/>
+
+    <BelezuraCard/>
+
+    <BelezuraCard/>
 
     </View>
   );
@@ -24,19 +46,29 @@ const TelaSecundaria = ({}) => {
 
 const style = StyleSheet.create({
   view: {
-    flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: '#ffffff'
+    color: '#ffffff',
+    padding: 10,
+    flexDirection: "row"
   },
   image: {
-  flex: 0.4,
-  height: 50,
-  width: 350
-  }
+    flex: 0.4,
+    height: 50,
+    width: 350, 
+  },
+  image2: {
+    height: 20,
+    width: 20 
+    },
+  text2: {
+    color: '#ffffff',
+    flexDirection: "row",
+    padding: 5
+    }
 });
 
 export default TelaSecundaria;

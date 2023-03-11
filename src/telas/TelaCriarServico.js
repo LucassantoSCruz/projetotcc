d10aca440d3c0159ea67fb91221062e92e357827
LucassantoSCruz <lucas.cruz162@etec.sp.gov.br>
2023-03-11 18:15:29 -0300

    Criar TelaCriarServico
    Criação da TelaCriarServico

---
+++ src/telas/TelaCriarServico.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
+
+
+const TelaCriarServico = () => {
+    return (
+        <View style={{flex: 1}}>
+            <ScrollView>
+            <View style={styles.fundoimagem}>
+                <TouchableOpacity style={styles.botaoimagem}>
+                    <Text>
+                        Trocar/Imagem
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+            <View>
+                <Text style={styles.textonome}>
+                    Nome do Serviço
+                </Text>
+                <TextInput style={styles.nomeservico}
+                placeholder='Digite o nome do serviço'
+                maxLength={35}
+                />
+            </View>
+
+        <View>
+            <Text style={styles.textocampo}>
+                Descrição
+            </Text>
+            <TextInput
+            style={styles.txtdescricao}
+            placeholder='Coloque uma descrição do serviço'
+            multiline={true}
+            numberOfLines={6}
+            maxLength={300}
+            />
+        </View>
+
+        <View style={styles.valor}>
+            <View style={{flexDirection: 'row', }}>
+                <Text style={styles.preco}>
+                    R$
+                </Text>
+                <TextInput
+                    style={styles.colocarpreco}
+                    placeholder='00,00'
+                    maxLength={6}
+                    keyboardType='numeric'
+                    />
+            </View>
+        </View>
+
+        <TouchableOpacity style={styles.botao}>
+            <Text style={styles.textobotao}>            
+                Comprar
+            </Text>
+        </TouchableOpacity>
+
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    fundoimagem:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#dcbadb',
+        height: 250,
+    },
+    botaoimagem: {
+        margin: 90,
+        borderWidth: 1,
+        borderColor: 'black',
+        padding: 20,
+        borderRadius: 20,
+        fontWeight: 'bold',
+        backgroundColor: 'grey',
+        opacity: '50%'
+    },
+    nomeservico: {
+        borderWidth: 1,
+        borderColor: 'black',
+        margin: 15,
+        padding: 15,
+        borderRadius: 20,
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    textonome: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 15,
+        marginLeft: 15
+    }, 
+    textocampo: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginLeft: 15
+    }, 
+    txtdescricao: {
+        color: 'grey',
+        fontSize: 15,
+        margin: 15,
+        borderWidth: 1,
+        borderColor: 'grey',
+        borderRadius: 20,
+        padding: 15
+    }, 
+    valor: {
+        flexDirection: 'row',
+        justifyContent:'space-between'
+    },
+    preco: {
+        fontSize: 30,
+        fontWeight:'bold',
+        color: '#9a6b99',
+        padding: 5,
+        marginLeft: 15,
+    },
+    colocarpreco: {
+        fontSize: 30,
+        fontWeight:'bold',
+        color: '#9a6b99',
+        padding: 5,
+        marginRight: 15,
+        borderWidth: 1,
+        borderColor: '#9a6b99',
+        borderRadius: 20
+    },
+    botao: {
+        backgroundColor: '#9a6b99',
+        padding: 10,
+        borderRadius: 100,
+        margin: 15
+    },
+    textobotao: {
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center'
+    }
+});
+
+export default TelaCriarServico

29f69757afd555534ae954944f1c6c7b49d44be2
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2023-03-13 04:47:21 -0300

    Update Mapa.js
    coloquei mais algumas modificações

--- src/componentes/Mapa.js
+++ src/componentes/Mapa.js
@@ -1,24 +1,21 @@
 import { useEffect, useState, useRef } from 'react';
 import { View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import { 
-    requestBackgroundPermissionsAsync, 
-    getCurrentPositionAsync,
-    LocationObject,
-    watchPositionAsync,
-    LocationAccuracy
-} from 'expo-location'
+import * as Location from 'expo-location'
+import cadastro from '../telas/TelaCadastro'
 
-
-
-export default function App(){
+const Mapa = () => {
 
     //definição de tipagem de estado
     const [location, setLocation] = useState<LocationObject | null>(null);
 
     const mapRef = useRef<MapView>(null);
 
+    const [ coordenadas, setCoordenadas ] = useState({latitude:0, longitude:0})
 
+    const [errorMsg,  setErrorMsg ] = useState(null);
+
+    
     async function requestLocationPermissions(){
         
         //estado de permissão
@@ -34,9 +31,21 @@ export default function App(){
     }
 
     useEffect(() => {
-        requestLocationPermissions();
+        //requestLocationPermissions();
+        //codigo add com o gpt
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if(status !== 'granted'){
+                setErrorMsg('Deu errro na permissão')
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })()
     }, []);
 
+    //mapa com as configuraçõe do video
     useEffect(() => {
         watchPositionAsync({
             accuracy: LocationAccuracy.Highest,
@@ -54,34 +63,45 @@ export default function App(){
         });
     }, []);
 
+
+    //obtendo as coordenadas do endereco
+    //geocoder ta no Back-End, é uma biblioteca do Node.js
+    const coordenadasEnd = async (endereco) =>{
+        const res = await geocoder.geocode(endereco);
+        const {latitude, longitude} = res[0];
+        setCoordenadas({latitude, longitude})
+    };
+
     return(
 
         <View style = {styles.container}>
 
         { 
-            location && 
-            <MapView
-                ref={mapRef}
-                style={styles.mapa}
-                initialRegion={{ 
-                    latitude: location.coords.latitude,
-                    longitude: location.coords.longitude,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-             }}
-            >
-                <Marker
-                    coordinate={{
-                        latitude:  location.coords.latitude ,
+            location ? ( 
+                <MapView
+                    ref={mapRef}
+                    style={styles.mapa}
+                    initialRegion={{ 
+                        latitude: location.coords.latitude,
                         longitude: location.coords.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
                 }}
-            />
-           
-            </MapView>
-        }
+                >
+                    <MapView.Marker
+                        coordinate={{
+                            latitude:  coordenadas.latitude ,
+                            longitude: coordenadas.longitude,
+                    }}
+                    title={endereco}
+                />
+                </MapView>
+            ): ( 
+                <Text> Procurando...</Text>
+            )}    
         </View>
     );
-}
+};
     
 
 //estilo
@@ -100,3 +120,5 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start'
     }
 });
+
+export default Mapa;

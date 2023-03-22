20a6eb3514e1de1e4963a2ae89f5b6dbeb427f77
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2023-03-22 15:44:40 -0300

    Create ClienteController.js

---
+++ controller/ClienteController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const RouteCliente = require('../routes/RouteClientes')
+const Controller = express.Router();
+
+
+Controller.post('/CadastroDadosF', RouteCliente.createCliente);
+Controller.get('Belezura/ListagemDados', RouteCliente.getCliente);
+Controller.get('Belezura/ListagemDadosID/:IDCliente', RouteCliente.getCliente);
+Controller.put('Belezura/AtualizarDados/:IDCliente', RouteCliente.putCliente);
+Controller.delete('Belezura/DeletarDados/:IDCliente', RouteCliente.destroyCliente);
+
+module.exports = Controller;

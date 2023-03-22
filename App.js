be728859b66216e4201184dc6fd9936a99784a1a
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2023-03-22 15:44:34 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import TelaInicial from './src/telas/TelaInicial';
 import TelaLogin from './src/telas/TelaLogin';
 import TabNavegacao from './src/Rotas';
 import TelaCadastro from './src/telas/TelaCadastro';
+import Mapa from './src/componentes/Mapa'
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +13,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Inicio">
-        <Stack.Screen name="Inicio" component={TelaInicial} options={{headerTitleAlign: 'center', title: 'Início'}}/>
+        <Stack.Screen name="Inicio" component={Mapa} options={{headerTitleAlign: 'center', title: 'Início'}}/>
         <Stack.Screen name="Login" component={TelaLogin} options={{headerTitleAlign: 'center'}}/>
         <Stack.Screen name="Cadastro" component={TelaCadastro} options={{headerTitleAlign: 'center'}}/>
         <Stack.Screen name="Profissionais" component={TabNavegacao} options={{headerShown: false}}/>

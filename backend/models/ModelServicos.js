25d7758df4a61d8a075bfdec2d4ade019c64d722
Giovanna <112666597+AC-Giovanna@users.noreply.github.com>
2023-04-27 19:45:29 -0300

    Atualização da model serviços

--- backend/models/ModelServicos.js
+++ backend/models/ModelServicos.js
@@ -13,6 +13,10 @@ const modelServicos = conexao.define('tbl_Servicos', {
         primaryKey: true,
         autoIncrement: true
     },
+    Imagem: {
+        type: sequelize.BLOB,
+        allowNull: true
+    },
     Preco: {
         type: sequelize.DECIMAL(10,2),
         allowNull: false
@@ -30,39 +34,6 @@ const modelServicos = conexao.define('tbl_Servicos', {
     createdAt: 'dataCriacao',
     updatedAt: 'ultimaModificacao'
 });
-
-//Relacionamentos
-
-//Importação das models
-const modelAgenda = require('./ModelAgenda');
-const modelAvaliacoes = require('./ModelAvaliacoes')
-const modelCategorias = require('./ModelCategorias');
-
-//Declaração dos relacionamentos
-modelServicos.hasMany(modelAgenda, {
-    foreignKey: 'FK_Servicos_Agenda'
-});
-modelAgenda.belongsTo(modelServicos, {
-    foreignKey: 'FK_Servicos_Agenda'
-});
-
-modelServicos.hasMany(modelAvaliacoes, {
-    foreignKey: 'FK_Servicos_Avaliacoes'
-});
-modelAvaliacoes.belongsTo(modelServicos, {
-    foreignKey: 'FK_Servicos_Avaliacoes'
-});
-
-modelServicos.belongsToMany(modelCategorias, {
-    through: 'servicos_categorias',
-    uniqueKey: 'FK_Servicos_Categorias',
-    timestamps: false
-});
-modelCategorias.belongsToMany(modelServicos, {
-    through: 'servicos_categorias',
-    uniqueKey: 'FK_Categorias_Servicos',
-    timestamps: false
-})
 
 //Exportação do modelo
 module.exports = modelServicos;    

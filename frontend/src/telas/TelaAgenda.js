ca029e0aa9e2257f27fc26fdf842c5b9b626d63f
Giovanna <112666597+AC-Giovanna@users.noreply.github.com>
2023-05-31 16:51:21 -0300

    Update TelaAgenda.js

--- frontend/src/telas/TelaAgenda.js
+++ frontend/src/telas/TelaAgenda.js
@@ -2,120 +2,174 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, RefreshControl, FlatList, Text, View, TouchableOpacity, Image, SafeAreaView, ScrollView} from 'react-native';
 import CaixaAgenda from '../componentes/CaixaAgenda';
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const TelaAgenda = () => {
 
-    const [agendamentos, setAgendamentos] = useState([])
-    const [refreshing, setRefreshing] = useState(false);
+  const [agendamentos, setAgendamentos] = useState([])
+  const [refreshing, setRefreshing] = useState(false);
+  const [profissionais, setProfissionais] = useState(null)
+  const [servicos, setServicos] = useState(null)
+  const [clientes, setClientes] = useState(null)
+  
+  // const obterDados = async () => {
+  //   try {
+  //     const valor = await AsyncStorage.getItem('tipoconta');
+  //     if (valor !== null) {
+  //       const tipoconta = JSON.parse(valor);
+  //       setTipoconta(tipoconta);
+  //       console.log("Tipo de conta: " + JSON.stringify(tipoconta))
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   } 
 
-    const fetchData = () => {
-        setTimeout(() => {
-            // Lógica para buscar os dados atualizados
-            infoAgendamentos()
+  //   if(tipoconta == 'Cliente'){
+  //     try {
+  //       const valor = await AsyncStorage.getItem('idUsuario');
+  //       if (valor !== null) {
+  //         const idUsuario = JSON.parse(valor);
+  //         setIdCliente(idUsuario);
+  //         console.log("Dados passados para tela (Cliente): " + JSON.stringify(idCliente))
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }else{
+  //     try {
+  //       const valor = await AsyncStorage.getItem('idUsuario');
+  //       if (valor !== null) {
+  //         const idUsuario = JSON.parse(valor);
+  //         setIdProfisional(idUsuario);
+  //         console.log("Dados passados para tela (Profissional): " + JSON.stringify(idProfissional))
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  // };
 
-            setRefreshing(false);
-        }, 2000);
-    };
+  const fetchData = () => {
+    setTimeout(() => {
+      // Lógica para buscar os dados atualizados
+      infoAgendamentos()
+      //obterDados()
 
-    const handleRefresh = () => {
-        setRefreshing(true);
-        fetchData();
-    };
+      setRefreshing(false);
+    }, 2000);
+  };
 
-    useEffect(() => {
-        infoAgendamentos()
-    }, []);
+  const handleRefresh = () => {
+    setRefreshing(true);
+    fetchData();
+  };
 
-    const infoAgendamentos = () => {
-      axios.get('http://192.168.1.11:3000/listagemAgendamentos')
-        .then(function (response) {
-            setAgendamentos(response.data)
-            //console.log(agendamentos.data)
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    }
+  useEffect(() => {
+    infoAgendamentos()
+    //obterDados()
+  }, []);
 
-    const infoProfissionais = () => {
+  const infoAgendamentos = () => {
+    axios.get('http://192.168.1.11:3000/listagemAgendamentos')
+    .then(function (response) {
+      setAgendamentos(response.data)
 
-    }
+      console.log('Agendamentos recuperados: ' + JSON.stringify(agendamentos.data))
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
 
-    const infoServicos = () => {
+  const infoProfissionais = () => {
+    axios.get(`http://192.168.1.11:3000/ListarProfissionalCNPJ/${agendamentos.item.FK_Profissionais_Agenda}`)
+    .then(function (response) {
+      setProfissionais(response.data)
 
-    }
+      console.log('Profissional recuperado: ' + JSON.stringify(profissionais))
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
 
-    return (
-        <SafeAreaView>
-          <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}>
-            <FlatList
-                horizontal={false}
-                data={agendamentos.data}
-                renderItem={({item}) => <CaixaAgenda item={item} />}
-                // keyExtractor={item => item.id}
-            />
-          </ScrollView>
-          
-        </SafeAreaView>
-      );
+  const infoServicos = () => {
+    axios.get(``)
+  }
+
+  const infoClientes = () => {
+    axios.get(``)
+  }
+
+  return (
+    <SafeAreaView>
+      <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}>
+        <FlatList
+          horizontal={false}
+          data={agendamentos.data}
+          renderItem={({item}) => <CaixaAgenda item={item} />}
+          // keyExtractor={item => item.id}
+        />
+      </ScrollView>  
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      marginTop: 50
-    },
-    caixa: {
-      width: 350,
-      height: 250,
-      backgroundColor: '#DCBADB',
-      borderRadius: 10,
-      borderColor: 'black',
-      borderWidth: 1
-    },
-    imgtxt: {
-      flexDirection: 'row',
-      borderWidth: 1,
-      borderRadius: 10,
-      borderColor: 'black',
-      width: 340,
-      height: 163,
-      marginTop: 10,
-      marginLeft: 5,
-      backgroundColor: '#F4E8F2'
-    },
-    imagem: { 
-      width: 165,
-      height: 160,
-      borderRadius: 10
-    },
-    texto: {
-      width: 165,
-      height: 160,
-      marginLeft: 5,
-      borderRadius: 10,
-    },
-    titulo: {
-      fontWeight: 'bold',
-      fontSize: 15
-    },
-    info: {
-      marginTop: 15
-    },
-    botao: {
-      alignItems:'center',
-      justifyContent: 'center',
-      borderColor: 'black',
-      backgroundColor: '#F4E8F2',
-      borderWidth: 1,
-      borderRadius: 10,
-      width: 110,
-      height: 60,
-      marginLeft: 5,
-    }
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginTop: 50
+  },
+  caixa: {
+    width: 350,
+    height: 250,
+    backgroundColor: '#DCBADB',
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1
+  },
+  imgtxt: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'black',
+    width: 340,
+    height: 163,
+    marginTop: 10,
+    marginLeft: 5,
+    backgroundColor: '#F4E8F2'
+  },
+  imagem: { 
+    width: 165,
+    height: 160,
+    borderRadius: 10
+  },
+  texto: {
+    width: 165,
+    height: 160,
+    marginLeft: 5,
+    borderRadius: 10,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+  info: {
+    marginTop: 15
+  },
+  botao: {
+    alignItems:'center',
+    justifyContent: 'center',
+    borderColor: 'black',
+    backgroundColor: '#F4E8F2',
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 110,
+    height: 60,
+    marginLeft: 5,
+  }
 });
 
 export default TelaAgenda

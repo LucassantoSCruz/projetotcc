2228a31833bd6984097895faf94fc759b0a5d0fb
AC-Giovanna <112666597+AC-Giovanna@users.noreply.github.com>
2023-06-13 22:28:32 -0300

    salvar horário e data corretamente

--- frontend/src/telas/TelaServico.js
+++ frontend/src/telas/TelaServico.js
@@ -1,16 +1,17 @@
 import { ENDERECO_API } from '../../config';
 import { StyleSheet, Text, View, Image, ScrollView, RefreshControl, TouchableOpacity, TextInput } from 'react-native';
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer, useRef } from 'react';
 import { BottomSheet } from 'react-native-btr';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import MaskInput, { Masks, createNumberMask } from 'react-native-mask-input';
 import { MaskedTextInput } from 'react-native-mask-text';  // esse é outro componente para máscarar o valor inserido, vou conferir ainda qual dos dois compensa mais usar
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Moment from 'moment';
 
-const TelaServico = ({navigation}) => {
+const TelaServico = ({ navigation }) => {
 
-    const route = useRoute()   
+    const route = useRoute()
     const [refreshing, setRefreshing] = useState(false);
     const [idServico, setIdServico] = useState(null)
     const [titulo, setTitulo] = useState(null)
@@ -18,20 +19,17 @@ const TelaServico = ({navigation}) => {
     const [preco, setPreco] = useState(null)
     const [precoFormatado, setPrecoFormatado] = useState(null);
     const [visivel, setVisivel] = useState(false);
-    // Campos para inserção do agendamento
     const [FK_Clientes_Agenda, setFK_Clientes_Agenda] = useState(null);
     const [FK_Profissionais_Agenda, setFK_Profissionais_Agenda] = useState(null);
     const [FK_Servicos_Agenda, setFK_Servicos_Agenda] = useState(null);
     const [data, setData] = useState('');
     const [horario, setHorario] = useState(null);
-    const [agendamento, setAgendamento] = useState(null)
-    const [valorTotal, setValorTotal] = useState(null);
 
     const fetchData = () => {
         setTimeout(() => {
-          // Lógica para buscar os dados atualizados
-          listarInfoServico(route.params.idServico)
-          setRefreshing(false); 
+            // Lógica para buscar os dados atualizados
+            listarInfoServico(route.params.idServico)
+            setRefreshing(false);
         }, 2000);
     };
 
@@ -58,33 +56,39 @@ const TelaServico = ({navigation}) => {
 
     const listarInfoServico = (idServico) => {
         axios.get(`${ENDERECO_API}/listarServicosID/${idServico}`)
-        .then(function (response){
-            setTitulo(response.data.data.titulo)
-            setDescricao(response.data.data.descricao)
-            setPreco(response.data.data.preco)
-            setPrecoFormatado(preco.replace(".",","))
-            setFK_Profissionais_Agenda(response.data.data.FK_Profissionais_Servicos)
-            setFK_Servicos_Agenda(response.data.data.ID)
-        })
-        .catch(function (error){
-            console.log(error)
-        })
+            .then(function (response) {
+                setTitulo(response.data.data.titulo)
+                setDescricao(response.data.data.descricao)
+                setPreco(response.data.data.preco)
+                setPrecoFormatado(preco.replace(".", ","))
+                setFK_Profissionais_Agenda(response.data.data.FK_Profissionais_Servicos)
+                setFK_Servicos_Agenda(response.data.data.ID)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
     }
 
     const enviarAgendamento = () => {
+        const dataFormatada = data.format('YYYY/MM/DD');
+        const agendamento = dataFormatada + ' ' + horario + ':00';
+        console.log(dataFormatada)
+        console.log(horario)
+        console.log(agendamento)
+
         axios.post(`${ENDERECO_API}/cadastrarAgendamento`, {
-            data: agendamento, 
-            FK_Servicos_Agenda, 
-            FK_Clientes_Agenda, 
+            data: agendamento,
+            FK_Servicos_Agenda,
+            FK_Clientes_Agenda,
             FK_Profissionais_Agenda
         })
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-            navigation.navigate('TelaPagamento')
-        })
-        .catch (function (erro) {
-            console.log(erro);
-        })
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                navigation.navigate('TelaPagamento')
+            })
+            .catch(function (erro) {
+                console.log(erro);
+            })
     }
 
     const obterDados = async () => {
@@ -196,14 +200,11 @@ const TelaServico = ({navigation}) => {
 
                             <View style={styles.campoinserir}>
 
-                            <MaskedTextInput
+                                <MaskedTextInput
                                     mask="99:99"
                                     onChangeText={(text, rawText) => {
                                         console.log(text);
-                                        setHorario(text)
-                                        setAgendamento(data + ' ' + horario)
-                                        console.log(agendamento)
-                                        // console.log(rawText);
+                                        setHorario(text);
                                     }}
                                     style={styles.campotexto}
                                     placeholder='__:__'
